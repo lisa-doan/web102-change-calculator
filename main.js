6dc49c17770ded34202d25a26f6bc868df9f4725
Lisa Doan <ldoan24@gmail.com>
2021-10-15 14:30:32 -0700

    Completed web102-change-calculator

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 // Write your JavaScript here
+
+const $calculateButton = document.getElementById("calculate-change");
+
+$calculateButton.addEventListener('click', calculateChange);
+
+function calculateChange() {
+  const $amountDue = document.getElementById("amount-due")
+  const $amountReceived = document.getElementById("amount-received")
+  const changeDue = $amountReceived.value - $amountDue.value;
+  let cents = changeDue * 100;
+
+  const dollars = Math.floor(cents/100);
+  cents -= dollars * 100;
+
+  const quarters = Math.floor((cents)/25);
+  cents -= quarters * 25;
+
+  const dimes = Math.floor((cents)/10);
+  cents -= dimes * 10;
+
+  const nickels = Math.floor((cents)/5);
+  cents -= nickels * 5;
+
+  const pennies = Math.round(cents/1);
+
+  document.getElementById("dollars-output").innerText = dollars
+  document.getElementById("quarters-output").innerText = quarters
+  document.getElementById("dimes-output").innerText = dimes
+  document.getElementById("nickels-output").innerText = nickels
+  document.getElementById("pennies-output").innerText = pennies
+
+}
